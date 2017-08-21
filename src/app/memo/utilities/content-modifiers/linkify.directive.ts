f7fec3568629d58164a4533f5d93d0a6eeaa7772
Frank Strieter <fs@co-it.eu>
2017-08-21 16:05:51 +0200

    (directive:likify) Fix setting of value after converting url

--- src/app/memo/utilities/content-modifiers/linkify.directive.ts
+++ src/app/memo/utilities/content-modifiers/linkify.directive.ts
@@ -1,38 +1,30 @@
-import { Directive, Renderer2, ElementRef, OnInit, HostListener } from '@angular/core';
+import { Directive, OnInit, HostListener } from '@angular/core';
+import { NgControl } from '@angular/forms';
 
 @Directive({
   selector: '[trLinkify]'
 })
 export class LinkifyDirective {
   urls = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-  hyperLinks = /<a.+><\/a>/;
-  fromHyperLinkToURL = /(<a.+href=".+" target="_blank">)(.+)(<\/a>)/
+  fromHyperLinkToURL = /(<a.+href=".+" target="_blank">)(.+)(<\/a>)/;
 
   constructor(
-    private renderer: Renderer2,
-    private element: ElementRef) { }
+    private control: NgControl) { }
 
   @HostListener('focus')
     convertFromLink() {
-      let content = this.element.nativeElement.value;
+      let content = this.control.control.value;
       content = content.replace(this.fromHyperLinkToURL, '$2');
-      this.renderer.setProperty(this.element.nativeElement, 'value', content);
+      this.control.control.setValue(content);
     }
 
   @HostListener('blur')
   convertToLink() {
-    const content = this.element.nativeElement.value;
-
+    const content = this.control.control.value;
     const foundUrls = content.match(this.urls);
-    console.log(foundUrls);
-
-    //const foundLinks = content.replace(this.hyperLinks, link => ;
-    //console.log(foundLinks);
-
     const contentWithLinks = content.replace(this.urls, url =>
       `<a contenteditable="false" href="${url}" target="_blank">${url}</a>`
     );
-
-     this.renderer.setProperty(this.element.nativeElement, 'value', contentWithLinks);
+    this.control.control.setValue(contentWithLinks);
   }
 }
